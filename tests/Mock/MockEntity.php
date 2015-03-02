52d38a4f6959051a1a356c39f7ddb46e69a3f221
yutakachiba <yutakachiba@gmail.com>
2015-03-02 18:59:03 +0900

    modify __get() when getXXX method defined in the Entity.

--- tests/Mock/MockEntity.php
+++ tests/Mock/MockEntity.php
@@ -17,9 +17,10 @@ use Genro\Domain\Entity\EntityTrait;
  * @package Genro\Domain\Mock
  * @author  Yutaka Chiba <yutakachiba@gmail.com>
  *
- * @property-read string $name
- * @property-read string $address
+ * @property-read string    $name
+ * @property-read string    $address
  * @property-read \DateTime $birthday
+ * @property-read \DateTime $created_at
  */
 class MockEntity implements EntityInterface
 {
@@ -41,15 +42,27 @@ class MockEntity implements EntityInterface
      */
     private $birthday;
 
+    /**
+     * @var \DateTime
+     */
+    private $created_at;
+
     public function __construct(
         $name,
         $address,
-        \DateTime $birthday
+        \DateTime $birthday,
+        \DateTime $created_at
     ) {
 
-        $this->name     = $name;
-        $this->address  = $address;
-        $this->birthday = $birthday;
+        $this->name       = $name;
+        $this->address    = $address;
+        $this->birthday   = $birthday;
+        $this->created_at = $created_at;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->created_at->format('Y-m-d H:i:s');
     }
 
     public function setBirthday(\DateTime $birthday)
