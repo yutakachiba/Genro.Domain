a453dbd0b75b99e8e9183847d526e6a7c5d0c2a2
yutakachiba <yutakachiba@gmail.com>
2015-03-18 07:36:14 +0900

    fix Entity

---
+++ src/Entity/Entity.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Genro\Domain\Entity
+ *
+ * @copyright Yutaka Chiba <yutakachiba@gmail.com>
+ * @created 2014/07/02 14:20
+ */
+namespace Genro\Domain\Entity;
+
+/**
+ * Interface Entity
+ *
+ * @package Genro\Domain\Entity
+ * @author yutakachiba@gmail.com
+ */
+interface Entity
+{
+}
